9debf57a0284de2ce88fe8b1d3ecc1558e72b48a
Hackercode152S C:\Users\mark\Desktop\basic_structure_and_syntax\JAVARICE git config --global user.name <cabatayclarence@gmail.com>
2026-06-17 13:43:28 +0800

    jva

---
+++ basic_structures_and_syntax.java
@@ -0,0 +1,11 @@
+package basic_structures_and_syntax;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("I'm mark cabatay!");
+        System.out.println("I'm 18 years old!");
+        System.out.println("I live in Caloocan City!");
+        System.out.println("I am a student at Grade 12 Peridot!");
+    }
+}
