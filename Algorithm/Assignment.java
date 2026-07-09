68f1ab59e2072c1728eccb546aa29dd680802dd3
Hackercode152S C:\Users\mark\Desktop\basic_structure_and_syntax\JAVARICE git config --global user.name <cabatayclarence@gmail.com>
2026-07-09 14:47:55 +0800

    1

---
+++ Algorithm/Assignment.java
@@ -0,0 +1,17 @@
+public class Assignment {
+    public static void main(String[] args) {
+        int a = 5; int b = 3;
+        System.out.println(b %= a);//a+15
+        System.out.println(a /= ++b);//a = 15 - 6 = 9
+        System.out.println(a *= a++);//a=45
+        System.out.println(a /= --b);//a=11
+        System.out.println(b %= a );//a=4
+
+        
+        
+        
+        
+
+    }
+}
+  
