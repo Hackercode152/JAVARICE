4195db8f59e99597c49cb3e70e23c473c82d6166
Hackercode152S C:\Users\mark\Desktop\basic_structure_and_syntax\JAVARICE git config --global user.name <cabatayclarence@gmail.com>
2026-07-27 12:45:32 +0800

    PRES

--- Algorithm/IfAndIfElse.java
+++ Algorithm/IfAndIfElse.java
@@ -1,20 +1,35 @@
-public class IfAndIfElse {
+public class IfAndIfElse { 
     public static void main(String[] args) {
 
-        int score = 85;
+        int score = 75;
+        boolean hasID = false;
+        char section = 'C';
 
-        // if statement
-        if (score >= 90) {
-            System.out.println("PASIKAT!");
+        if (hasID) {
+            System.out.println("Student is allowed to enter.");
         }
 
-        // if-else statement
-        if (score >= 75) {
-            System.out.println("MAGALING!");
+        if (score >= 80) {
+            System.out.println("Status: Passed");
         } else {
-            System.out.println("UWI KANA.");
+            System.out.println("Status: Failed");
         }
 
+        switch (section) {
+            case 'A':
+                System.out.println("Section A - Diamond");
+                break;
+
+            case 'B':
+                System.out.println("Section B - Emerald");
+                break;
+
+            case 'C':
+                System.out.println("Section C - Peridot");
+                break;
+
+            default:
+                System.out.println("Unknown Section");
+        }
     }
 }
-
