c32c66bc08ab0bb8dafdae40978039d3c0cd09fd
Hackercode152S C:\Users\mark\Desktop\basic_structure_and_syntax\JAVARICE git config --global user.name <cabatayclarence@gmail.com>
2026-07-30 14:38:05 +0800

    drinkmenu

--- Algorithm/drinksmenu.java
+++ Algorithm/drinksmenu.java
@@ -9,7 +9,7 @@ public class drinksmenu {
         System.out.println("===== DRINK MENU =====");
         System.out.println("1. Alfonso");
         System.out.println("2. Emperador");
-        System.out.println("3. Black label");
+        System.out.println("3. Black Label");
 
         System.out.print("Choose your drink (1-3): ");
         int choice = input.nextInt();
@@ -20,22 +20,25 @@ public class drinksmenu {
                 System.out.println("Drink: Alfonso");
                 System.out.println("Flavor: PLATINUM");
                 System.out.println("Price: ₱500");
+                System.out.println("DRINK HARD!!!");
                 break;
 
             case 2:
                 System.out.println("Drink: Emperador");
                 System.out.println("Flavor: CLVB");
                 System.out.println("Price: ₱800");
+                System.out.println("DRINK HARD!!!");
                 break;
 
             case 3:
-                System.out.println("Drink: Johnnie Walker ");
-                System.out.println("Flavor: Black label");
+                System.out.println("Drink: Johnnie Walker");
+                System.out.println("Flavor: Black Label");
                 System.out.println("Price: ₱999");
+                System.out.println("DRINK HARD!!!");
                 break;
 
             default:
-                System.out.println("Idiot! just choose from 1 to 3.");
+                System.out.println("Invalid choice! Please choose from 1 to 3.");
         }
 
         input.close();
